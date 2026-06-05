c09f598a2004ce464e4bc3e7a0bc1a90d6173413
Yassir <atertay@gmail.com>
2026-06-05 08:06:23 +0100

    feat: Adding toast notification

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,23 @@ import { Route, Router } from '@solidjs/router';
 import { render } from 'solid-js/web';
 import App from './App';
 import './index.css';
+
+import { Toaster } from 'solid-sonner';
 import CreateNote from './pages/create-note';
 import NotFound from './pages/not-found';
 import NotePage from './pages/note-page';
 
 render(
   () => (
-    <Router>
-      <Route path="/" component={App} />
-      <Route path="/create" component={CreateNote} />
-      <Route path="/notes/:id" component={NotePage} />
-      <Route path="*" component={NotFound} />
-    </Router>
+    <>
+      <Router>
+        <Route path="/" component={App} />
+        <Route path="/create" component={CreateNote} />
+        <Route path="/notes/:id" component={NotePage} />
+        <Route path="*" component={NotFound} />
+      </Router>
+      <Toaster richColors />
+    </>
   ),
   document.getElementById('root')!,
 );
