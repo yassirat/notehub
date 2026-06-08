faaab3429cbd97992269064e6aa659d39897f5f5
Yassir <atertay@gmail.com>
2026-06-08 15:38:49 +0100

    refactor: Add an about route

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css';
 
 import { Toaster } from 'solid-sonner';
+import About from './pages/about';
 import CreateNote from './pages/create-note';
 import NotFound from './pages/not-found';
 import NotePage from './pages/note-page';
@@ -15,6 +16,7 @@ render(
       <Router>
         <Route path="/" component={App} />
         <Route path="/create" component={CreateNote} />
+        <Route path="/about" component={About} />
         <Route path="/notes/:id" component={NotePage} />
         <Route path="*" component={NotFound} />
       </Router>
