2de8de8793a506a32f11d108dea8cfbfca747b43
Yassir <atertay@gmail.com>
2026-06-11 17:09:45 +0100

    Fix some build issues

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,9 +5,9 @@ import { createEffect, createSignal, Match, Show, Switch } from 'solid-js';
 import pencil from '/pencil_3075908.png';
 
 interface HeaderProps {
-  setIsDeleteModalOpen: (value: boolean) => void;
-  isEdited: () => boolean; // It's a function
-  updateNote: () => void;
+  setIsDeleteModalOpen?: (value: boolean) => void;
+  isEdited?: () => boolean;
+  updateNote?: () => void;
 }
 
 export default function Header(props: HeaderProps) {
@@ -30,77 +30,79 @@ export default function Header(props: HeaderProps) {
   });
 
   return (
-    <>
-      <header class="py-4 px-4">
-        <nav class="max-w-3xl mx-auto flex justify-between items-center sticky top-0">
-          <Switch>
-            {/* Home route */}
-            <Match when={location.pathname === '/'}>
-              <div class="flex items-center gap-2">
-                <img src={pencil} alt="logo" class="w-8" />
-                <h1 class="text-xl font-bold hidden md:block">NotoHub</h1>
-              </div>
-              <div class="flex items-center gap-4">
-                <A
-                  href="/about"
-                  aria-label="Go to about"
-                  class="text-sm font-medium fade dark:text-white">
-                  About
-                </A>
+    <header class="py-4 px-4">
+      <nav class="max-w-3xl mx-auto flex justify-between items-center sticky top-0">
+        <Switch>
+          {/* Home route */}
+          <Match when={location.pathname === '/'}>
+            <div class="flex items-center gap-2">
+              <img src={pencil} alt="logo" class="w-8" />
+              <h1 class="text-xl font-bold hidden md:block">NotoHub</h1>
+            </div>
+            <div class="flex items-center gap-4">
+              <A
+                href="/about"
+                aria-label="Go to about"
+                class="text-sm font-medium fade dark:text-white">
+                About
+              </A>
+              <button
+                type="button"
+                onClick={toggleDarkMode}
+                title="Change theme"
+                aria-label="Change theme"
+                class="p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-900">
+                {darkMode() ? (
+                  <Sun size={18} strokeWidth={2.5} fill="true" />
+                ) : (
+                  <Moon size={18} strokeWidth={2.5} fill="true" />
+                )}
+              </button>
+            </div>
+          </Match>
+
+          {/* Create route */}
+          <Match when={location.pathname === '/create'}>
+            <A href="/" aria-label="Go back">
+              <MoveLeft size={24} strokeWidth={2.5} />
+            </A>
+          </Match>
+
+          {/* About route */}
+          <Match when={location.pathname === '/about'}>
+            <A href="/" aria-label="Go back">
+              <MoveLeft size={24} strokeWidth={2.5} />
+            </A>
+          </Match>
+
+          {/* Note detail route */}
+          <Match when={location.pathname.startsWith('/notes/')}>
+            <A href="/" aria-label="Go back">
+              <MoveLeft size={24} strokeWidth={2.5} />
+            </A>
+            <div class="flex gap-4">
+              <Show when={props.isEdited?.()}>
                 <button
                   type="button"
-                  onClick={toggleDarkMode}
-                  aria-label="Change theme"
-                  class="p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-900">
-                  {darkMode() ? (
-                    <Sun size={18} strokeWidth={2.5} fill="true" />
-                  ) : (
-                    <Moon size={18} strokeWidth={2.5} fill="true" />
-                  )}
+                  onClick={props.updateNote}
+                  title="Edit note"
+                  aria-label="Edit note"
+                  class="bg-lime-700 hover:bg-lime-600 text-white text-sm transition-colors p-2 rounded-lg fade">
+                  <CheckIcon size={16} strokeWidth={2.5} />
                 </button>
-              </div>
-            </Match>
-
-            {/* Create route */}
-            <Match when={location.pathname === '/create'}>
-              <A href="/" aria-label="Go back">
-                <MoveLeft size={24} strokeWidth={2.5} />
-              </A>
-            </Match>
-
-            {/* About route */}
-            <Match when={location.pathname === '/about'}>
-              <A href="/" aria-label="Go back">
-                <MoveLeft size={24} strokeWidth={2.5} />
-              </A>
-            </Match>
-
-            {/* Note detail route */}
-            <Match when={location.pathname.startsWith('/notes/')}>
-              <A href="/" aria-label="Go back">
-                <MoveLeft size={24} strokeWidth={2.5} />
-              </A>
-              <div class="flex gap-4">
-                <Show when={props.isEdited()}>
-                  <button
-                    onClick={props.updateNote}
-                    aria-label="Edit note"
-                    class="bg-lime-700 hover:bg-lime-600 text-white text-sm transition-colors p-2 rounded-lg fade">
-                    <CheckIcon size={16} strokeWidth={2.5} />
-                  </button>
-                </Show>
-                <button
-                  onClick={() => props.setIsDeleteModalOpen(true)}
-                  title="delete note"
-                  aria-label="Delete note"
-                  class="bg-red-700 text-white hover:bg-red-600 transition-colors duration-200 p-2 rounded-lg fade">
-                  <Trash2Icon size={16} strokeWidth={2.5} />
-                </button>
-              </div>
-            </Match>
-          </Switch>
-        </nav>
-      </header>
-    </>
+              </Show>
+              <button
+                type="button"
+                onClick={() => props.setIsDeleteModalOpen?.(true)}
+                title="Delete note"
+                aria-label="Delete note"
+                class="bg-red-700 text-white hover:bg-red-600 transition-colors duration-200 p-2 rounded-lg fade">
+                <Trash2Icon size={16} strokeWidth={2.5} />
+              </button>
+            </div>
+          </Match>
+        </Switch>
+      </nav>
+    </header>
   );
 }
