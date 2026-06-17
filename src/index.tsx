8d606fe02529d416b0609be7c2b33c41f25752f1
Yassir <atertay@gmail.com>
2026-06-17 09:01:31 +0100

    Remove about route

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 /* @refresh reload */
-import { Route, Router } from '@solidjs/router';
-import { render } from 'solid-js/web';
-import App from './App';
-import { SignContextProvider } from './context/sign-context';
-import './index.css';
-import About from './pages/about';
-import CreateNote from './pages/create-note';
-import NotFound from './pages/not-found';
-import NotePage from './pages/note-page';
+import { Route, Router } from "@solidjs/router";
+import { render } from "solid-js/web";
+import App from "./App";
+import { SignContextProvider } from "./context/sign-context";
+import "./index.css";
+import CreateNote from "./pages/create-note";
+import NotFound from "./pages/not-found";
+import NotePage from "./pages/note-page";
 
-import { Toaster } from 'solid-sonner';
-import PrivateRoute from './components/private-route';
-import { NotesProvider } from './context/note-context';
-import RegisterPage from './pages/register-page';
-import UserProfile from './pages/user-profile';
+import { Toaster } from "solid-sonner";
+import PrivateRoute from "./components/private-route";
+import { NotesProvider } from "./context/note-context";
+import RegisterPage from "./pages/register-page";
+import UserProfile from "./pages/user-profile";
 
 render(
   () => (
@@ -31,7 +30,6 @@ render(
           <Route path="/register" component={RegisterPage} />
           <Route path="/user" component={UserProfile} />
           <Route path="/create" component={CreateNote} />
-          <Route path="/about" component={About} />
           <Route path="/notes/:id" component={NotePage} />
           <Route path="*" component={NotFound} />
         </Router>
@@ -39,5 +37,5 @@ render(
       </NotesProvider>
     </SignContextProvider>
   ),
-  document.getElementById('root')!,
+  document.getElementById("root")!,
 );
