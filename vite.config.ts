28241a656bbfa7af8037d319dc7f5c40ed5e08e3
Yassir <atertay@gmail.com>
2026-06-17 09:04:17 +0100

    file prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import solid from 'vite-plugin-solid';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid(), tailwindcss()],
