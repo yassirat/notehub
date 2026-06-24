910a7fbb0e9324342ed9f9650bc4485d19bd3610
Yassir <atertay@gmail.com>
2026-06-24 14:41:46 +0100

    style: Add skeleton loading

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,17 @@
-import { A } from '@solidjs/router';
-import { PlusIcon } from 'lucide-solid';
-import { For, Show } from 'solid-js';
-import Header from '../components/header';
-import { useNotes } from '../context/note-context';
+import { A } from "@solidjs/router";
+import { PlusIcon } from "lucide-solid";
+import { For, Show } from "solid-js";
+import Header from "../components/header";
+import { SkeletonLoading } from "../components/loading";
+import { useNotes } from "../context/note-context";
 
 export default function Home() {
   const { notes, loading } = useNotes();
 
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = date.toLocaleDateString('en-US', { month: 'long' });
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = date.toLocaleDateString("en-US", { month: "long" });
     const year = date.getFullYear();
 
     return `${day} ${month} ${year}`;
@@ -21,34 +22,37 @@ export default function Home() {
   // };
 
   return (
-    <article class="min-h-dvh bg-neutral-100 dark:bg-neutral-950 dark:text-white grid grid-rows-[auto_1fr] font-main">
+    <article class="font-main grid min-h-dvh grid-rows-[auto_1fr] bg-neutral-100 dark:bg-neutral-950 dark:text-white">
       <Header />
 
-      <section class="relative flex flex-col gap-4 w-full px-6 mt-8 max-w-xl mx-auto">
+      <section class="relative mx-auto mt-8 flex w-full max-w-xl flex-col gap-4 px-6">
         <Show
           when={loading()}
           fallback={
             <Show
               when={notes().length > 0}
               fallback={
-                <div class="text-center py-10 space-y-4 place-items-center">
+                <div class="place-items-center space-y-4 py-10 text-center">
                   <p class="font-medium text-gray-800 dark:text-gray-600">
                     No notes yet. Create one to get started!
                   </p>
                   <A
                     href="/create"
                     aria-label="Create a new note"
-                    class="bg-neutral-900 text-white dark:bg-neutral-200 dark:text-black p-2 md:p-3 rounded-lg font-medium text-xs transition-colors duration-200 hover:bg-neutral-700 dark:hover:bg-neutral-300 fade flex items-center gap-2">
+                    class="fade flex items-center gap-2 rounded-lg bg-neutral-900 p-2 text-xs font-medium text-white transition-colors duration-200 hover:bg-neutral-700 md:p-3 dark:bg-neutral-200 dark:text-black dark:hover:bg-neutral-300"
+                  >
                     <PlusIcon size={16} strokeWidth={2.5} />
                     Create
                   </A>
                 </div>
-              }>
+              }
+            >
               <div class="grid gap-4">
                 <A
                   href="/create"
                   aria-label="Create a new note"
-                  class="bg-neutral-900 text-white dark:bg-neutral-200 dark:text-black px-3 py-2 rounded-lg font-medium text-xs transition-colors duration-200 hover:bg-neutral-700 dark:hover:bg-neutral-300 fade ml-auto flex items-center gap-2">
+                  class="fade ml-auto flex items-center gap-2 rounded-lg bg-neutral-900 px-3 py-2 text-xs font-medium text-white transition-colors duration-200 hover:bg-neutral-700 dark:bg-neutral-200 dark:text-black dark:hover:bg-neutral-300"
+                >
                   <PlusIcon size={14} strokeWidth={2.5} />
                   Create
                 </A>
@@ -56,14 +60,15 @@ export default function Home() {
                   {(note) => (
                     <A
                       href={`/notes/${note.id}`}
-                      class="rounded-md p-4 bg-neutral-50 dark:bg-black transition-transform duration-200 hover:-translate-y-1 fade shadow-note dark:shadow-note-dark">
+                      class="fade shadow-note dark:shadow-note-dark rounded-md bg-neutral-50 p-4 transition-transform duration-200 hover:-translate-y-1 dark:bg-black"
+                    >
                       <h2 class="font-semibold capitalize">{note.title}</h2>
-                      <p class="text-neutral-800 dark:text-neutral-400 text-sm font-medium">
+                      <p class="text-sm font-medium text-neutral-800 dark:text-neutral-400">
                         {note.description.length > 40
-                          ? `${note.description.split('\n')[0].slice(0, 40)}...`
-                          : note.description.split('\n')[0]}
+                          ? `${note.description.split("\n")[0].slice(0, 40)}...`
+                          : note.description.split("\n")[0]}
                       </p>
-                      <div class="flex justify-between items-center">
+                      <div class="flex items-center justify-between">
                         <p class="text-xs font-medium text-neutral-600">
                           {formatDate(note.updated_at || note.created_at)}
                         </p>
@@ -73,8 +78,9 @@ export default function Home() {
                 </For>
               </div>
             </Show>
-          }>
-          <div class="text-center font-medium py-10">Loading notes...</div>
+          }
+        >
+          <SkeletonLoading />
         </Show>
       </section>
     </article>
